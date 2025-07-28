f68d75a7524750689fcf44cad1d4c223a5bb3d80
UnicornDad8 <cecibenitezca@gmail.com>
2025-07-28 14:37:29 -0300

    Add Home title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { ThemeCotext } from "../context/ThemeContextProvider";
+import { FaChevronDown } from "react-icons/fa6";
+
 const Home = () => {
+  const { theme } = useContext(ThemeCotext);
+
   return (
-    <div className="w-full h-full py-3 px-7 leght:bg-white dark:bg-gray-800 mt-[60px]">
-      <p className="xl:ml-[230px] text-gray-900 dark:text-gray-200">Home</p>
+    <div className="w-full h-full py-3 pt-[60px] px-7 leght:bg-white dark:bg-gray-800 mt-[60px] flex items-center">
+     <div className="flex w-full xl:w-[calc(100vw - 530px)] xl:ml-[230px] xl:mr-[300px] justify-between">
+     <div className="flex items-center">
+      <h3 className="text-sm font-semibold text-gray-900 dark:text-gray-200">Overview</h3>
+     </div>
+     <div className="flex items-center gap-2">
+      <h3 className="text-sm text-gray-900 dark:text-gray-200">Today</h3>
+      <FaChevronDown size={12} color={theme === "light" ? "rgba(0, 0, 0, 0.6)" : "rgba(255, 255, 255, 0.6)"} />
+     </div>
+     </div>
     </div>
   )
 }
